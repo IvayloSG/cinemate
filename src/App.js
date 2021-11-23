5ee612f925df77d48c05ace288efe3b6b4d38fcf
IvayloSG <ivailosg@gmail.com>
2021-11-24 01:24:50 +0200

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Header from "./components/Header/Header"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app-container">
+      <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,21 @@
+import "./Header.css"
+
 function Header() {
-    return;
+    return (
+        <nav className="header-nav">
+            <section className="header-nav-container">
+                <h2 className="header-nav-container-logo">Cinemate</h2>
+                <ul className="header-nav-container-list">
+                    <li className="header-nav-container-list-element">Home</li>
+                    <li className="header-nav-container-list-element">Explore</li>
+                    <li className="header-nav-container-list-element">MyPlace</li>
+                    <li className="header-nav-container-list-element logout">Logout</li>
+                    <li className="header-nav-container-list-element">Login</li>
+                    <li className="header-nav-container-list-element">Register</li>
+                </ul>
+            </section>
+        </nav>
+    );
 }
 
 export default Header;
